d69f253cd7a9e0ea737b3a6243f7fef75a3e17c9
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-05-24 10:12:09 +0200

    Hover Effects

--- sproute-meals-frontend/src/Auth/Login.js
+++ sproute-meals-frontend/src/Auth/Login.js
@@ -1,31 +1,56 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './styles/index.css'
 import Logo from './styles/logo.png'
 import {Link } from 'react-router-dom'
 import login from './styles/login.svg'
 function Login() {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('');
+    const getEmail =(e)=>{
+        setEmail(e.target.value);
+    }
+    const getPassword =(e)=>{
+        setPassword(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const LoginData = async ()=>{
+            const PostData = await fetch('http://196.223.240.154:8099/supapp/api/auth/signin',{
+                method : 'POST',
+                headers:{
+                  'Content-Type':'application/json'
+                },
+                body : JSON.stringify({
+                  login:email,
+                  password : password,
+                })
+              })
+              console.log('Login Data',PostData);
+        }
+        LoginData()
+    }
   return (
-    <div class='container'>
-        <div class='logo_container'>
-            <div class='name'>
-                <img src={`${Logo}`} alt="Logo for us" class="logo"/>
+    <div className='container'>
+        <div className='logo_container'>
+            <div className='name'>
+                <img src={`${Logo}`} alt="Logo for us" className="logo"/>
                 <h1 className="sprout pt-3 text-[#FA4b0c] text-2xl">PROUT</h1>
               <h1 className="sprout pt-4 pl-1">Meals</h1>
             </div>
-            <div class='icon'>
+            <div className='icon'>
                 <img src={`${login}`} alt='signup'/>
             </div>
         </div>
-        <div class="form_part">
-            <form>
+        <div className="form_part">
+            <form onSubmit={handleSubmit}>
                 <h1 className='pb-5 font-bold text-xl'>Login</h1>
                 <div class="set">
                     <label id="email" className='labels'>Email</label>
-                    <input type='email' placeholder="Email Address" required />
+                    <input type='email' onChange={getEmail} value={email} placeholder="Email Address" required />
                 </div>
                 <div class="set">
                     <label id="password" className='labels'> Password</label>
-                    <input type='password' placeholder="Password" required />
+                    <input type='password' onChange={getPassword} value={password} placeholder="Password" required />
                 </div>
                 <button type="submit" class="submit"><Link to='/overview'>Login</Link></button>
                 <p className='para bottom-0'>All rights reserved 2022</p>
--- sproute-meals-frontend/src/components/Home/Getstarted.js
+++ sproute-meals-frontend/src/components/Home/Getstarted.js
@@ -13,27 +13,27 @@ const GetStarted = () => {
             </div>
             <div className="flex flex-row mt-10">
                 <div className="grid grid-cols-2">
-                    <div className=" bg-white rounded-md mx-10 my-5 p-10">
-                        <img src={Component1} className="w-20 h-16 m-auto" alt="" />
+                    <div className=" bg-white hover:bg-[#FA4B0C] hover:text-white hover:cursor-pointer duration-300 rounded-md mx-10 my-5 p-10">
+                        <img src={Component1} className="w-20 h-[5em] m-auto" alt="" />
                         <p className="font-bold text-base m-auto">Register your account</p>
                     </div>
-                    <div className=" bg-white rounded-md mx-10 my-5 p-10">
-                        <img src={Component2} className="w-20 h-16 m-auto" alt="" />
+                    <div className="  bg-white hover:bg-[#FA4B0C] hover:text-white hover:cursor-pointer duration-300 rounded-md mx-10 my-5 p-10">
+                        <img src={Component2} className="w-20 h-[5em] m-auto" alt="" />
                         <p className="font-bold text-base">Create your status</p>
                     </div>
-                    <div className=" bg-white rounded-md mx-10 m-auto my-5 p-10">
-                        <img src={Component3} alt="" className="w-20 h-16 m-auto" />
+                    <div className="  bg-white hover:bg-[#FA4B0C] hover:text-white hover:cursor-pointer duration-300 rounded-md mx-10 my-5 p-10">
+                        <img src={Component3} alt="" className="w-20 h-[5em] m-auto" />
                         <p className="font-bold text-base">Start Receiving orders</p>
                     </div>
-                    <div className=" bg-white rounded-md mx-10 m-auto my-5 p-10">
-                        <img src={Component4} alt="" className="w-20 h-16 m-auto" />
+                    <div className="  bg-white hover:bg-[#FA4B0C] hover:text-white hover:cursor-pointer duration-300 rounded-md mx-10 my-5 p-10">
+                        <img src={Component4} alt="" className="w-20 h-[5em] m-auto" />
                         <p className="font-bold text-base mx-auto w-fit">Manage Your deliveries</p>
                     </div>
                 </div>
                 <div className="w-1/2 ml-20 mt-10">
-                    <p className="p-16">Britain is famous all over the world for its gardens, dogs, villages, buses, cars, private schools, the Queen, sports like football and cricket, Parliament. However, not so much has been written about the wonderful food that can be found in Britain. Read two texts about food and say what the English eat and what the British food is like. Is there any difference between the English and the British food? The texts are for beginners and for the upper-intermediate level.</p>
+                    <p className="pr-10 py-6">Britain is famous all over the world for its gardens, dogs, villages, buses, cars, private schools, the Queen, sports like football and cricket, Parliament. However, not so much has been written about the wonderful food that can be found in Britain. Read two texts about food and say what the English eat and what the British food is like. Is there any difference between the English and the British food? The texts are for beginners and for the upper-intermediate level.</p>
                     {/* <button className="bg-[#FA4B0C] mx-auto  rounded px-8 py-3 ml-16 text-white font-bold">Get Started</button> */}
-                    <button className="bg-[#FA4B0C] text-white w-fit mx-auto px-5"><Link to='/login'>Get Started</Link></button>
+                    <button className=" bg-[#FA4B0C] text-white p-4 rounded-lg font-semibold hover:bg-transparent hover:scale-75 hover:border-2 hover:border-solid duration-500 hover:border-[#FA4B0C] hover:text-[#FA4B0C] w-fit mx-auto px-6"><Link to='/login'>Get Started</Link></button>
                 </div>
             </div>
         </>
